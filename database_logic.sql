a5a6d247982a2a1fa22c56e1817cfce3fdd74cae
Abdulrahman Itman <abdoitman@gmail.com>
2023-04-22 03:45:52 +0200

    added triggers for orders and order_details tables

--- database_logic.sql
+++ database_logic.sql
@@ -1,3 +1,39 @@
--- Create triggers for te database
+-- Create triggers for the database
+
+DROP TRIGGER IF EXISTS orders_before_insert;
+DROP TRIGGER IF EXISTS orders_detalis_after_insert;
+DROP TRIGGER IF EXISTS notification_is_recieved;
 
 USE warehouse_data;
+
+-- before an order is placed, make the orderdate = time of insertion 
+DELIMITER //
+CREATE TRIGGER orders_before_insert BEFORE INSERT ON orders
+FOR EACH ROW
+BEGIN
+	SET NEW.OrderDate = now();
+END//
+
+-- after an order is placed:
+-- 1) set the corrosponding shelf isHavingOrder to True
+-- 2) decrement ItemsInStock for each product in the order
+CREATE TRIGGER orders_detalis_after_insert AFTER INSERT ON orders_details
+FOR EACH ROW
+BEGIN
+    UPDATE Shelves
+    SET Shelves.isHavingOrder = True
+    WHERE Shelves.ProductID = NEW.ProductID;
+    
+	UPDATE Products
+	SET Products.ItemsInStock = Products.ItemsInStock - NEW.Quantity
+	WHERE Products.ProductID = NEW.ProductID;
+END//
+
+-- when  a notificaiton is recieved, make the notificationDateTime = time of insertion 
+CREATE TRIGGER notification_is_recieved BEFORE INSERT ON Notifications
+FOR EACH ROW
+BEGIN
+	set NEW.NotificationDateTime = now();
+END//
+
+DELIMITER ;
